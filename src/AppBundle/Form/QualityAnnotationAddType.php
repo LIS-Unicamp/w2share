d1340520cbc976ec0c8aec9f654fa72f47ba6a39
joana <joana@10.1.1.115>
2017-01-31 10:13:51 -0200

    QualityFlow: viewer for quality annotation

---
+++ src/AppBundle/Form/QualityAnnotationAddType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class QualityAnnotationAddType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder
+            ->add('value', 'text', array('label' => 'Value'))
+            ->add('save', 'submit', array(
+                    'label' => 'Add',
+                    'attr' => array('class' => 'btn blue')
+                ))
+        ;
+    }
+    
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\QualityAnnotation',
+        ));
+    }
+
+    public function getName()
+    {
+        return 'qualityannotation';
+    }
+    
+}
