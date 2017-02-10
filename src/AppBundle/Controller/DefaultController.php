b30a78a747977e57a7c58b98511bd9294aea3788
Lucas Carvalho <lucasaugustomcc@gmail.com>
2017-02-10 11:04:35 -0200

    new About page

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,19 +4,28 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {                      
     /**
      * @Route("/", name="homepage")
      */
-    public function homepageAction(Request $request)
+    public function homepageAction()
     {           
         return $this->render('default/index.html.twig', array(
             
         ));
-    }     
+    }   
+    
+    /**
+     * @Route("/about", name="about")
+     */
+    public function aboutAction()
+    {           
+        return $this->render('default/about.html.twig', array(
+            
+        ));
+    }   
 }
 
 
